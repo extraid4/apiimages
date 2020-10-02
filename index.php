e3ba96c5bc3c23b37b62cc2437839a7a89288c92
extraid4 <72155505+extraid4@users.noreply.github.com>
2020-10-03 00:19:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<html>
-<body>
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
-
-</body>
-</html>
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "Mickey Mouse\n";
+fwrite($myfile, $txt);
+$txt = "Minnie Mouse\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
